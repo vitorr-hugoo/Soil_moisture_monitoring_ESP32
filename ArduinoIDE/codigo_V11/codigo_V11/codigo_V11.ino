95f8b8e42be313ae41ad19268565bf2e0af5e9af
Vitor Hugo Pavanelli <vitor.hugo128@gmail.com>
2024-10-12 19:19:00 -0300

    Atualização sensores

--- ArduinoIDE/codigo_V11/codigo_V11/codigo_V11.ino
+++ ArduinoIDE/codigo_V11/codigo_V11/codigo_V11.ino
@@ -1,7 +1,7 @@
 /*------- Código desenvolvido para um sistema de irrigação caseira com foco no cultivo de morangos-------
   ------- Vitor Hugo Pavanelli dos Reis -----------------------------------------------------------------
-  ------- Versão 8 - 30/09/2024 -------------------------------------------------------------------------
-  ------- Versão Final ---------------------------------------------------------------------
+  ------- Versão 10 - 12/10/2024 -------------------------------------------------------------------------
+  ------- V.11 ---------------------------------------------------------------------
 */
 //---------Bibliotecas
 #include <WiFi.h>
@@ -9,16 +9,23 @@
 #include <WebServer.h>
 #include <ESPmDNS.h>
 #include <esp_task_wdt.h>
+#include <DHT.h>
+
+#define DHT11_PIN  13
 
 /*---------------- Lógica -------------------------------------------------------------------------------------------------------
 -> 4 níveis: encharcado, adequado, submolhado, seco
 
-      encharcado  400 - 1199  -> se colocar 0 pode significar que não ta lendo direito
-      Adequado   1200 - 1399
-      Submolhado 1400 - 1999
-      Solo seco  2000+
----------------------------------------------------------------------------------------------------------------------------------*/
+      encharcado  500 - 1049  -> se colocar 0 pode significar que não ta lendo direito
+      Adequado   1050 - 1399
+      Submolhado 1400 - 1899
+      Solo seco  1900 - 3200
+---------------------------------------------------------------------------------------------------------------------------------
+-> calcular a vazao p saber o tempo certo que a bomba fica ligada
+-> integrar um dh11 e aproveitar a umidade do ar p maior precisao
 
+
+*/
 //--------------- Variáveis Globais------------------------------------------------------------------------------------------------
 int valor_sensor_1=0;
 int valor_sensor_2=0;
@@ -33,18 +40,79 @@ const int pino_sensor_2 = 33; // Sensor 2
 const int pino_sensor_3 = 36; // Sensor 3
 const int pino_sensor_4 = 39; // Sensor 4
 
-//Wifi config
-const char* ssid = "xxxxxxxxxxxxxx"; //ssid
-const char* password = "xxxxxxxxxx"; //password
+//--------------- Variáveis da bomba ------------------------------------------------------------------------------------------
 
-//IP config 
-IPAddress local_IP(10, 0, 0, 123);
-IPAddress gateway(10, 0, 0, 1);
+const int pino_bomba = 4; // ver certinho
+
+//--------------- DHT11 ----------------------------------------------------------------------------------------------------------
+
+DHT dht11(DHT11_PIN, DHT11);
+float Temperatura=0.00;
+float UmidadeAr=0.00;
+
+
+void configuraWatchdog(int timeoutSegundos);
+void alimentaWatchdog();
+void handleRoot();
+void aquisicaoDados();
+int media(int sensor1, int sensor2, int sensor3, int sensor4);
+String statusSensor();
+float aquisicaoUmidade();
+void leituraTemperatura();
+void leituraUmidade();
+
+const char* ssid = "Vitor";
+const char* password = "03102001";
+
+IPAddress local_IP(192, 168, 0, 63);
+IPAddress gateway(192, 168, 0, 1);
 IPAddress subnet(255, 255, 255, 0);
-
 
 //Configura a porta do servidor -> porta 80
 WebServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(pino_sensor_1, INPUT);
+  pinMode(pino_sensor_2, INPUT);
+  pinMode(pino_sensor_3, INPUT);
+  pinMode(pino_sensor_4, INPUT);
+  pinMode(pino_bomba, OUTPUT);
+  //pinMode(dhtPin, INPUT);
+  configuraWatchdog(15);  // Configura o Watchdog Timer com timeout de 15 segundos
+
+  WiFi.mode(WIFI_STA);
+  WiFi.config(local_IP, gateway, subnet);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("Connected to WiFi");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp32")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+  aquisicaoDados();
+  media(valor_sensor_1,valor_sensor_2, valor_sensor_3, valor_sensor_4);
+  server.handleClient();  // Lida com requisições HTTP
+  delay(2);               // Pequeno atraso para evitar consumo excessivo de CPU
+  leituraTemperatura();
+  leituraUmidade();
+  //delay(5000); //5 seg com a bomba ativada
+  acionaBomba(pino_bomba);
+  alimentaWatchdog();      // Alimenta o Watchdog Timer
+}
 
 // Estrutura de configuração para o Watchdog Timer
 esp_task_wdt_config_t wdt_config;
@@ -107,7 +175,8 @@ void aquisicaoDados() {
   Serial.println(valor_sensor_2);
   Serial.println(valor_sensor_3);
   Serial.println(valor_sensor_4);
-  Serial.println("-------------------------------------------------");*/
+  Serial.println("-------------------------------------------------");
+  */
 }
 
 int media(int sensor1, int sensor2, int sensor3, int sensor4){
@@ -117,51 +186,45 @@ int media(int sensor1, int sensor2, int sensor3, int sensor4){
 }
 
 String statusSensor() {
-  if (media_sensores >= 2000) return "Seco";
-  if (media_sensores > 1400) return "Submolhado";
-  if (media_sensores > 1200) return "Adequado";
-  if (media_sensores > 400) return "Encharcado";
+  if (media_sensores >= 1900) return "Seco";
+  if (media_sensores > 1399) return "Submolhado";
+  if (media_sensores > 1049) return "Adequado";
+  if (media_sensores > 499) return "Encharcado";
   return "Problemas";
 }
 
 float aquisicaoUmidade() {
-  return map(media_sensores, 1000, 3100, 900, 0) / 10.0;
+  return map(media_sensores, 800, 3200, 100, 0);
 }
 
-void setup() {
-  Serial.begin(115200);
-  pinMode(pino_sensor_1, INPUT);
-  pinMode(pino_sensor_2, INPUT);
-  pinMode(pino_sensor_3, INPUT);
-  pinMode(pino_sensor_4, INPUT);
-
-  configuraWatchdog(15);  // Configura o Watchdog Timer com timeout de 15 segundos
-
-  WiFi.mode(WIFI_STA);
-  WiFi.config(local_IP, gateway, subnet);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+void acionaBomba(int pino_bomba){
+  if (media_sensores >= 1900) {
+    digitalWrite(pino_bomba, LOW); // Liga a bomba
+    delay(5000); // Mantém a bomba ligada por 5 segundos
   }
-
-  Serial.println("Connected to WiFi");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  if (MDNS.begin("esp32")) {
-    Serial.println("MDNS responder started");
+  else if (media_sensores >= 1500 && media_sensores < 1900) {
+    digitalWrite(pino_bomba, LOW); // Liga a bomba
+    delay(2500); // Mantém a bomba ligada por 2,5 segundos
+  } 
+  else {
+    digitalWrite(pino_bomba, HIGH); // Desliga a bomba
   }
-
-  server.on("/", handleRoot);
-  server.begin();
-  Serial.println("HTTP server started");
 }
 
-void loop() {
-  aquisicaoDados();
-  media(valor_sensor_1,valor_sensor_2, valor_sensor_3, valor_sensor_4);
-  server.handleClient();  // Lida com requisições HTTP
-  delay(2);               // Pequeno atraso para evitar consumo excessivo de CPU
-  alimentaWatchdog();      // Alimenta o Watchdog Timer
+void leituraTemperatura(){
+  Temperatura=dht11.readHumidity();
+  Serial.print("Temperature: ");
+    Serial.print(Temperatura);
+    Serial.println("°C ");
+    Serial.println("---------------------------- ");
+    
 }
+
+void leituraUmidade(){
+  UmidadeAr=dht11.readTemperature();
+  Serial.print("Humidity: ");
+    Serial.print(UmidadeAr);
+    Serial.println("%");
+    Serial.println("---------------------------- ");
+}
+
